83bfdb19dc87e2862062ed44fe8e6bc5dd7c43ae
Famstyle <famstylefreedom@gmail.com>
2018-10-28 15:57:16 +0700

    เปลี่ยนชื่อ title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
 
 
 
-    <title>HOME</title>
+    <title>Room-Reservation</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
