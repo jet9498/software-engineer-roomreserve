ea1f79f8d500ba798e68e6be28e34e487deee997
Famwithjourney <famstylefreedom@gmail.com>
2018-12-03 13:09:58 +0700

    แก้555

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,7 +48,10 @@
             <li><a href="#section1">รายการห้อง</a></li>
             <li><a href="#" data-toggle="modal" data-target="#fam">ข้อปฏิบัติ</a></li>
             <li><a href="#section2">ติดต่อเรา</a></li>
+           
+
           @else
+             <li><a href="#" data-toggle="modal" data-target="#register">สร้างไอดี</a></li>
 
           @endif
           
@@ -68,6 +71,81 @@
       </div>
     </div>
   </nav>
+
+<div class="modal fade " id="register" role="dialog" style="z-index: 9999">
+  <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
   <div class="modal fade " id="id01" role="dialog" style="z-index: 9999">
     <!--ล็อคอินของ laravel user-->
   <div class="container">
@@ -183,6 +261,7 @@
                                 <button type="submit" class="btn btn-primary">
                                     สร้างห้อง
                                 </button>
+                                 
                                 
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,5 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
+Route::get('/register', 'Auth\RegisterController@createt');
 Auth::routes();
