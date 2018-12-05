8a746a256f284d30e733047a3cffd0a46a8ed43f
Famwithjourney <famstylefreedom@gmail.com>
2018-12-06 01:35:56 +0700

    eiei

--- database/migrations/2018_12_05_171746_create_datetable_table.php
+++ database/migrations/2018_12_05_171746_create_datetable_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableTable extends Migration
+class CreateDatetableTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateTableTable extends Migration
      */
     public function up()
     {
-      Schema::create('table', function (Blueprint $table) {
+      Schema::create('datetable', function (Blueprint $table) {
           $table->increments('TableID');
           $table->integer('roomID');
-          $table->string('Subject');
-          $table->string('Name');
-          $table->string('Day');
-          $table->time('TableStart');
-          $table->time('TableEnd');
+          $table->date('EndTerm');
           $table->timestamps();
       });
     }
@@ -32,6 +28,6 @@ class CreateTableTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table');
+        Schema::dropIfExists('datetable');
     }
 }
